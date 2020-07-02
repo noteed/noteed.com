5310fef2490d1c81d2b5870c07221bc729fc5dcb
Vo Minh Thu <thu@hypered.io>
2020-07-02 22:35:31 +0200

    Add missing not-os files.

--- site/default.nix
+++ site/default.nix
@@ -56,17 +56,7 @@ in rec
   html.blog.expose-local-server     = to-html md.blog.expose-local-server;
   html.blog.starting-with-nixops-1  = to-html md.blog.starting-with-nixops-1;
   html.blog.starting-with-nixops-2  = to-html md.blog.starting-with-nixops-2;
-  html.not-os.index = to-html md.not-os.index;
-  html.not-os.runvm = to-html md.not-os.runvm;
-  html.not-os.initrd = to-html md.not-os.initrd;
-  html.not-os.kernel = to-html md.not-os.kernel;
-  html.not-os.rootfs = to-html md.not-os.rootfs;
-  html.not-os.ext4 = to-html md.not-os.ext4;
-  html.not-os.stage-1 = to-html md.not-os.stage-1;
-  html.not-os.stage-2 = to-html md.not-os.stage-2;
-  html.not-os.dist = to-html md.not-os.dist;
-  html.not-os.extra-utils = to-html md.not-os.extra-utils;
-  html.not-os.path = to-html md.not-os.path;
+  html.not-os = builtins.mapAttrs (_: v: to-html v) md.not-os;
   html.notes = builtins.mapAttrs (_: v: to-html v) md.notes;
 
   html.all = pkgs.runCommand "all" {} ''
@@ -80,8 +70,8 @@ in rec
     cp ${html.blog.starting-with-nixops-2} $out/blog/starting-with-nixops-2.html
     cp ${html.not-os.index} $out/not-os/index.html
     cp ${html.not-os.runvm} $out/not-os/runvm.html
-    cp ${html.not-os.initrd} $out/not-os/initrd.html
     cp ${html.not-os.kernel} $out/not-os/kernel.html
+    cp ${html.not-os.initrd} $out/not-os/initrd.html
     cp ${html.not-os.rootfs} $out/not-os/rootfs.html
     cp ${html.not-os.ext4} $out/not-os/ext4.html
     cp ${html.not-os.stage-1} $out/not-os/stage-1.html
@@ -89,6 +79,16 @@ in rec
     cp ${html.not-os.dist} $out/not-os/dist.html
     cp ${html.not-os.extra-utils} $out/not-os/extra-utils.html
     cp ${html.not-os.path} $out/not-os/path.html
+    cp ${html.not-os.shrunk} $out/not-os/shrunk.html
+    cp ${html.not-os.toplevel} $out/not-os/toplevel.html
+    cp ${html.not-os.boot} $out/not-os/boot.html
+    cp ${html.not-os.raw} $out/not-os/raw.html
+    cp ${html.not-os.qcow2} $out/not-os/qcow2.html
+    cp ${html.not-os.syslinux} $out/not-os/syslinux.html
+    cp ${html.not-os.cmdline} $out/not-os/cmdline.html
+    cp ${html.not-os.root-modules} $out/not-os/root-modules.html
+    cp ${html.not-os.todo} $out/not-os/todo.html
+    cp ${html.not-os.digital-ocean} $out/not-os/digital-ocean.html
     cp ${html.notes.psu} $out/notes/psu.html
     ${pkgs.bash}/bin/bash ${replace-md-links} $out
   '';
