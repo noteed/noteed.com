c9a19c70ef4da7c17884b8792511fbc24bd68386
Vo Minh Thu <thu@hypered.io>
2021-03-17 22:04:02 +0100

    Add /videos page.

--- site/default.nix
+++ site/default.nix
@@ -20,12 +20,14 @@ let
    sha256 = "0n49i35x2jcirqi2w7qk5hbwi9z98xh3dnx3jr3gwzkw6lf1jq9g";
   };
 
-  design-system-version = "da8585ecaa62c00d5e32b490581ef41ee09d79d5";
+  videos = ../../videos;
+
+  design-system-version = "51694bea56d2e5c9545d88b35f11ccffbc536742";
   design-system = pkgs.fetchFromGitHub {
     owner = "hypered";
     repo = "design-system";
     rev = design-system-version;
-    sha256 = "1za6hf9ba7wvd0d2y4g0zn7vnvkfqmhpgq5sipc7yj7cxq24b941";
+    sha256 = "0wxvwwhj2xwhflnv02jffim4h6jgwziybv82z2mifmjczbvxhizn";
   };
   inherit (import design-system {}) template lua-filter replace-md-links static;
 
@@ -67,22 +69,29 @@ in rec
   md.blog.starting-with-nixops-2  = (import blog).starting-with-nixops-2;
   md.not-os = ((import not-os {}).site {}).md;
   md.notes = (dirsToMds ../notes);
+  md.videos = ((import videos).site {}).md;
+  svg.videos = ((import videos).site {}).svg;
 
   # nix-build site/ -A html.index
   html.index = to-html md.index;
   html.blog = builtins.mapAttrs (_: v: to-html v) md.blog;
   html.not-os = builtins.mapAttrs (_: v: to-html v) md.not-os;
   html.notes = builtins.mapAttrs (_: v: to-html v) md.notes;
+  html.videos = builtins.mapAttrs (_: v: to-html v) md.videos;
 
   html.all = pkgs.runCommand "all" {} ''
     mkdir -p $out/blog
     mkdir -p $out/not-os
     mkdir -p $out/notes
+    mkdir -p $out/videos/01
+
     cp ${html.index} $out/index.html
+
     cp ${html.blog.index} $out/blog/index.html
     cp ${html.blog.expose-local-server} $out/blog/expose-local-server.html
     cp ${html.blog.starting-with-nixops-1} $out/blog/starting-with-nixops-1.html
     cp ${html.blog.starting-with-nixops-2} $out/blog/starting-with-nixops-2.html
+
     cp ${html.not-os.index} $out/not-os/index.html
     cp ${html.not-os.runvm} $out/not-os/runvm.html
     cp ${html.not-os.kernel} $out/not-os/kernel.html
@@ -107,6 +116,10 @@ in rec
     cp ${html.notes.psu} $out/notes/psu.html
     cp ${html.notes.mini} $out/notes/mini.html
     cp ${html.notes.til} $out/notes/til.html
+
+    cp ${html.videos.index} $out/videos/index.html
+    sed "s@url('../../images/fonts/@url('/static/fonts/@" ${svg.videos.title-01} > $out/videos/01/title.svg
+
     ${pkgs.bash}/bin/bash ${replace-md-links} $out
   '';
 
